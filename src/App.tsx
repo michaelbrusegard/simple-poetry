6ea80e69a41b4f691bffbffe51f80809cfde1734
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-09-17 02:57:29 +0200

    feat: add card and select components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { Button } from './components/ui/Button'
+import { Select, SelectOption } from './components/ui/Select'
 
 function App() {
   return (
@@ -14,6 +15,11 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <Button>Hello</Button>
+      <Select value='1' onChange={() => {}}>
+        <SelectOption value='1'>One</SelectOption>
+        <SelectOption value='2'>Two</SelectOption>
+        <SelectOption value='3'>Three</SelectOption>
+      </Select>
     </>
   )
 }
