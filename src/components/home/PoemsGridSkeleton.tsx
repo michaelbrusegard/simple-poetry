f0826c5e1588b7e2ddee9f363be0f9e064fab323
sandefu <sandefu@stud.ntnu.no>
2024-09-17 14:16:14 +0200

    chore: update fetch amounts

--- src/components/home/PoemsGridSkeleton.tsx
+++ src/components/home/PoemsGridSkeleton.tsx
@@ -4,7 +4,7 @@ import { Skeleton } from '../ui/Skeleton'
 function PoemsGridSkeleton() {
   return (
     <div className={styles.grid}>
-      {Array.from({ length: 9 }).map((_, index) => (
+      {Array.from({ length: 12 }).map((_, index) => (
         <Skeleton className={styles.card} key={index} />
       ))}
     </div>
