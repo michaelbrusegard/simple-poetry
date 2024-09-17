aa7c90b07c2ac1778cd7c7804235b427685ab5be
nikolath <nikolath@stud.ntnu.no>
2024-09-17 16:31:26 +0200

    feat: create spinner and poem page

--- src/pages/poem/index.tsx
+++ src/pages/poem/index.tsx
@@ -1,12 +1,20 @@
+import styles from './Poem.module.css'
 import { useParams } from 'react-router-dom'
+import { Poem } from '../../components/poem/Poem'
+import { Suspense } from 'react'
+import { Spinner } from '../../components/ui/Spinner'
 
 export default function PoemPage() {
   const { poemName = '' } = useParams()
   const decodedPoemName = decodeURIComponent(poemName)
+  const poemAuthor = sessionStorage.getItem('selectedAuthor') || ''
 
   return (
-    <div>
+    <article>
       <h1>{decodedPoemName}</h1>
-    </div>
+      <Suspense fallback={<Spinner className={styles.spin} />}>
+        <Poem poemTitle={decodedPoemName} poemAuthor={poemAuthor} />
+      </Suspense>
+    </article>
   )
 }
