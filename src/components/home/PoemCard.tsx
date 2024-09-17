fd69579b77168426a4265606091a0ed4f073b34d
nikolath <nikolath@stud.ntnu.no>
2024-09-17 15:46:44 +0200

    feat: Link card to individual pages

---
+++ src/components/home/PoemCard.tsx
@@ -0,0 +1,29 @@
+import styles from './PoemCard.module.css'
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '../ui/Card'
+import { Link } from 'react-router-dom'
+import { Poem } from './types'
+
+function PoemCard({
+  poem,
+  ...props
+}: { poem: Poem } & React.HTMLAttributes<HTMLAnchorElement>) {
+  return (
+    <Link to={`/poem/${encodeURIComponent(poem.title)}`} {...props}>
+      <Card className={styles.card}>
+        <CardHeader>
+          <CardTitle>{poem.title}</CardTitle>
+        </CardHeader>
+        <CardContent>{poem.author}</CardContent>
+        <CardFooter>{poem.linecount} lines</CardFooter>
+      </Card>
+    </Link>
+  )
+}
+
+export { PoemCard }
