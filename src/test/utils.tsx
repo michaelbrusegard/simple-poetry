6def6a8e4ea5d342edb98289380e4ad95bd9e357
sandefu <sandefu@stud.ntnu.no>
2024-09-19 01:23:13 +0200

    test: add router support to tests

--- src/test/utils.tsx
+++ src/test/utils.tsx
@@ -1,5 +1,6 @@
 import { render } from '@testing-library/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter } from 'react-router-dom'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -11,7 +12,9 @@ const queryClient = new QueryClient({
 
 const customRender = (children: React.ReactElement) =>
   render(
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>,
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>{children}</BrowserRouter>
+    </QueryClientProvider>,
   )
 
 export { customRender as render }
