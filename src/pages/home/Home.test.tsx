19a5d4862cf316f4c13dbecfabeee31c218f4fdc
michbrus <michbrus@stud.ntnu.no>
2024-09-18 16:12:44 +0200

    test: add a render wrapper with query client

--- src/pages/home/Home.test.tsx
+++ src/pages/home/Home.test.tsx
@@ -1,4 +1,5 @@
-import { screen, render } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { render } from '../../test/utils'
 import HomePage from './index'
 
 describe('Home tests', () => {
