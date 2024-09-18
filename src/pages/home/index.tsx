c81943cea4679b91937a2a51d26d00abb202388c
sandefu <sandefu@stud.ntnu.no>
2024-09-18 19:03:59 +0200

    chore: add wrapper for some menu items

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -25,22 +25,24 @@ export default function HomePage() {
     <>
       <h1>Find Poetry</h1>
       <header className={styles.header}>
-        <Input
-          className={styles.searchBar}
-          placeholder='Search for a poem...'
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        ></Input>
-        <Select
-          value={selectedAuthor}
-          onChange={(e) => setSelectedAuthor(e.target.value)}
-        >
-          <SelectOption value='blake'>Blake</SelectOption>
-          <SelectOption value='dickinson'>Dickinson</SelectOption>
-          <SelectOption value='rossetti'>Rossetti</SelectOption>
-          <SelectOption value='shakespeare'>Shakespeare</SelectOption>
-          <SelectOption value='whitman'>Whitman</SelectOption>
-        </Select>
+        <div className={styles.searchWrapper}>
+          <Select
+            value={selectedAuthor}
+            onChange={(e) => setSelectedAuthor(e.target.value)}
+          >
+            <SelectOption value='blake'>Blake</SelectOption>
+            <SelectOption value='dickinson'>Dickinson</SelectOption>
+            <SelectOption value='rossetti'>Rossetti</SelectOption>
+            <SelectOption value='shakespeare'>Shakespeare</SelectOption>
+            <SelectOption value='whitman'>Whitman</SelectOption>
+          </Select>
+          <Input
+            className={styles.searchBar}
+            placeholder='Search for a poem...'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          ></Input>
+        </div>
       </header>
       <Suspense fallback={<PoemsGridSkeleton />}>
         <PoemsGrid searchTerm={searchTerm} selectedAuthor={selectedAuthor} />
