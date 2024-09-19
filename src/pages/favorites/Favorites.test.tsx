c67d59cfa7fe5d7f6d96a40737ac81c709bcb4cc
nikolath <nikolath@stud.ntnu.no>
2024-09-19 06:03:43 +0200

    test: favorites page

---
+++ src/pages/favorites/Favorites.test.tsx
@@ -0,0 +1,15 @@
+import { screen } from '@testing-library/react'
+import { render } from '../../test/utils'
+import FavoritesPage from './index'
+
+describe('FavoritesPage tests', () => {
+  it('should render the h1 element with the correct text', () => {
+    render(<FavoritesPage />)
+    expect(screen.getByText('Favorites')).toBeInTheDocument()
+  })
+
+  it('should render the link component correctly', () => {
+    render(<FavoritesPage />)
+    expect(screen.getByText('Home')).toHaveAttribute('href', '/project1/')
+  })
+})
