7879e6775ee8c468900f099c0326ca2493df3f59
nikolath <nikolath@stud.ntnu.no>
2024-09-19 12:58:20 +0200

    test: mock all test data

--- src/pages/home/Home.test.tsx
+++ src/pages/home/Home.test.tsx
@@ -1,11 +1,12 @@
-import { screen, fireEvent } from '@testing-library/react'
+import { screen, fireEvent, act } from '@testing-library/react'
 import { render } from '../../test/utils'
 import HomePage from './index'
 
 describe('Home tests', () => {
-  it('should render the title', () => {
-    render(<HomePage />)
-
+  it('should render the title', async () => {
+    await act(async () => {
+      render(<HomePage />)
+    })
     expect(
       screen.getByRole('heading', {
         level: 1,
@@ -17,18 +18,18 @@ describe('Home tests', () => {
     sessionStorage.clear()
   })
 
-  it('should render the select components with the correct initial value', () => {
+  it('should render the select components with the correct initial value', async () => {
     render(<HomePage />)
     expect(screen.getByDisplayValue('Shakespeare')).toBeInTheDocument()
     expect(screen.getByDisplayValue('Grid')).toBeInTheDocument()
   })
 
-  it('should render the input component with the correct initial value', () => {
+  it('should render the input component with the correct initial value', async () => {
     render(<HomePage />)
     expect(screen.getByPlaceholderText('Search for a poem...')).toHaveValue('')
   })
 
-  it('should render the link component correctly', () => {
+  it('should render the link component correctly', async () => {
     render(<HomePage />)
     expect(screen.getByText('Favorites')).toHaveAttribute(
       'href',
@@ -36,11 +37,11 @@ describe('Home tests', () => {
     )
   })
 
-  it('should update sessionStorage when state changes', () => {
+  it('should update sessionStorage when state changes', async () => {
     render(<HomePage />)
     fireEvent.change(screen.getByDisplayValue('Shakespeare'), {
-      target: { value: 'blake' },
+      target: { value: 'Emily Dickinson' },
     })
-    expect(sessionStorage.getItem('selectedAuthor')).toBe('blake')
+    expect(sessionStorage.getItem('selectedAuthor')).toBe('Emily Dickinson')
   })
 })
