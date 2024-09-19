7611ed77d9d995ac1f5147a43a2e83679ee56e78
nikolath <nikolath@stud.ntnu.no>
2024-09-19 05:57:40 +0200

    test: home page

--- src/pages/home/Home.test.tsx
+++ src/pages/home/Home.test.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react'
+import { screen, fireEvent } from '@testing-library/react'
 import { render } from '../../test/utils'
 import HomePage from './index'
 
@@ -11,5 +11,36 @@ describe('Home tests', () => {
         level: 1,
       }),
     ).toHaveTextContent('Find Poetry')
+  })
+
+  beforeEach(() => {
+    sessionStorage.clear()
+  })
+
+  it('should render the select components with the correct initial value', () => {
+    render(<HomePage />)
+    expect(screen.getByDisplayValue('Shakespeare')).toBeInTheDocument()
+    expect(screen.getByDisplayValue('Grid')).toBeInTheDocument()
+  })
+
+  it('should render the input component with the correct initial value', () => {
+    render(<HomePage />)
+    expect(screen.getByPlaceholderText('Search for a poem...')).toHaveValue('')
+  })
+
+  it('should render the link component correctly', () => {
+    render(<HomePage />)
+    expect(screen.getByText('Favorites')).toHaveAttribute(
+      'href',
+      '/project1/favorites',
+    )
+  })
+
+  it('should update sessionStorage when state changes', () => {
+    render(<HomePage />)
+    fireEvent.change(screen.getByDisplayValue('Shakespeare'), {
+      target: { value: 'blake' },
+    })
+    expect(sessionStorage.getItem('selectedAuthor')).toBe('blake')
   })
 })
